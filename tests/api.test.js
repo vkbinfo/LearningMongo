60b6cbf6f7e3aad435e6cdfe8f53a41c5dc72d32
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-12-11 22:13:56 +0530

    added testcases for getting all the todos or for route GET /todos

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,14 +1,22 @@
 const expect = require('expect');
 const request = require('supertest'); // a library for testing express app
 
-const {app} = require('../server/server');
-const {USER} = require('../models/users');
-const {TODO} = require('../models/todo');
+const {
+    app
+} = require('../server/server');
+const {
+    USER
+} = require('../models/users');
+const {
+    TODO
+} = require('../models/todo');
 
 describe('POST /user/new', () => {
     const testEmail = 'test@gmail.com';
     beforeEach((done) => {
-        USER.deleteMany({email:testEmail},(err) => {
+        USER.deleteMany({
+            email: testEmail
+        }, (err) => {
             if (err) {
                 return done(err);
             }
@@ -30,7 +38,9 @@ describe('POST /user/new', () => {
                 if (error) {
                     return done(error);
                 }
-                USER.find({email:testEmail}).then((doc) => {
+                USER.find({
+                    email: testEmail
+                }).then((doc) => {
                     expect(doc[0].email).toBe(testEmail);
                     done();
                 })
@@ -41,7 +51,9 @@ describe('POST /user/new', () => {
 describe('POST /todo/new', () => {
     const testText = 'Be a better asshole person.';
     beforeEach((done) => {
-        TODO.deleteMany({text:testText},(err) => {
+        TODO.deleteMany({
+            text: testText
+        }, (err) => {
             if (err) {
                 return done(err);
             }
@@ -65,12 +77,48 @@ describe('POST /todo/new', () => {
                 if (error) {
                     return done(error);
                 }
-                TODO.find({text:testText}).then((doc) => {
+                TODO.find({
+                    text: testText
+                }).then((doc) => {
                     expect(doc[0].text).toBe(testText);
                     expect(doc[0].completedAt).toBe(null);
                     expect(doc[0].completed).toBe(false);
                     done();
                 })
+            })
+    })
+})
+
+// test for getting all the todos from the database
+describe('GET /todos', () => {
+    beforeEach((done) => {
+        TODO.deleteMany().then((docs) => {}).catch((error) => {});
+        done();
+    })
+
+
+    it('should get all the todos in the response body', (done) => {
+        const newTodos = [{
+            text: 'It is something'
+        }, {
+            text: 'It is not something'
+        }]
+        TODO.insertMany(newTodos).then((docs) => {})
+            .catch((error) => {
+                console.log('failed insertion of records in the database for GET /todos testing', error)
+                done();
+            });
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.todos.length).toBe(2);
+            })
+            .end((error, result) => {
+                if (error) {
+                    return done(error);
+                }
+                done();
             })
     })
 })
