27af3a2316c103144bdf7310a4bada3a2074cd33
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-12-29 07:58:05 +0530

    added deletion and insertion in seed file for testing

---
+++ tests/seed.js
@@ -0,0 +1,61 @@
+const { ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const APP_SECRET = 'authsecretthatrequiresforJWTTokens';
+const {
+    USER
+} = require('../models/user');
+const {
+    TODO
+} = require('../models/todo');
+
+const todos = [{
+    _id: new ObjectId(),
+    text: 'It is something'
+}, {
+    _id: new ObjectId(),
+    text: 'It is not something'
+}]
+
+const userOneObjectId = new ObjectId();
+const userTwoObjectId = new ObjectId();
+const users = [
+    {
+        _id: userOneObjectId,
+        email: 'Vikash@gmail.com',
+        password: '12345678',
+        tokens: [
+            {
+                access: 'auth',
+                token: jwt.sign({ _id: userOneObjectId.toHexString(), access: 'auth' }, APP_SECRET).toString()
+            }
+        ]
+    },
+    {
+        _id: userTwoObjectId,
+        email: 'Akash@gmail.com',
+        password: 'abc45678',
+    }
+]
+
+
+const populateUsers = (done) => {
+    USER.deleteMany().then( () => {
+        USER.insertMany(users).then(() => {
+            done();
+        })
+    }).catch(() => {
+        console.error('Something failing while deleting and inserting user data in seed file')
+    }) 
+}
+
+const populateTodos = (done) => {
+    TODO.deleteMany().then( () => {
+        TODO.insertMany(todos).then(() => {
+            done();
+        })
+    }).catch(() => {
+        console.error('Something failing while deleting and inserting todo data in seed file')
+    })   
+}
+module.exports = { todos, users, populateUsers, populateTodos }
